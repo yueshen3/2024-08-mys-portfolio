071fb2b25c2a65f606b6b870fe96c9e829031ff2
Yueshen <68021595+yueshen3@users.noreply.github.com>
2024-10-01 22:55:35 -0500

    Update mysProjectName.js

--- script/mysProjectName.js
+++ script/mysProjectName.js
@@ -1,22 +1,13 @@
 import {dataProjects} from './mysData.js';
 import {dataDates} from './mysData.js';
+import {dataStartYear} from './mysData.js';
+import {dataStartMonth} from './mysData.js';
+import {dataScalerUnit} from './mysData.js';
+
 
 const groupDataDates = Object.groupBy(dataDates, ({ projectID }) => projectID);
 const arrayGroupDataDates = Object.keys(groupDataDates).map((e) => [e, groupDataDates[e]]);
-console.log(arrayGroupDataDates);
-
-const startYear = '2009';
-const startMonth = '1';
-const scalerUnit = 5;
-const endYear = '2028';
-const endMonth = '12';
-
-// const time = [];
-// let recordYear = startYear;
-// for (let i = 0; i <= endYear - startYear; i++) {
-//   time[i] = recordYear;
-//   recordYear ++;
-// }
+// console.log(arrayGroupDataDates);
 
 function getTime (e, i) {
   return e['phaseMonth'].split('/')[i];
@@ -27,19 +18,18 @@ for (let e of arrayGroupDataDates) {
   // let levelAA = e[0];
   let levelAB = [];
   for (let f of e[1]) {
-    levelAB.push(Number((getTime(f, 2) - startYear)) * 12 + 
-    Number(getTime(f, 0)) - Number(startMonth)); 
+    levelAB.push(Number((getTime(f, 2) - dataStartYear)) * 12 + 
+    Number(getTime(f, 0)) - Number(dataStartMonth)); 
   }
   levelB.push(levelAB);
 }
 
 let modifiedDataProjects = dataProjects;
 modifiedDataProjects.forEach((e, i) => {
-  Object.defineProperties (e, {'startPoint' : {value: levelB[i][0] * scalerUnit}})
-  Object.defineProperties (e, {'endPoint' : {value: scalerUnit * (Math.max(...levelB[i]) + 1)}})
+  Object.defineProperties (e, {'startPoint' : {value: levelB[i][0] * dataScalerUnit}})
+  Object.defineProperties (e, {'endPoint' : {value: dataScalerUnit * (Math.max(...levelB[i]) + 1)}})
 })
-
-console.log(modifiedDataProjects);
+// console.log(modifiedDataProjects);
 
 let htmlProjectName = '';
 let htmlProjectSvg = '';
@@ -51,10 +41,3 @@ for (let e of modifiedDataProjects) {
 
 document.querySelector('.projects-name-ul').innerHTML = htmlProjectName;
 document.querySelector('.projects-svg-ul').innerHTML = htmlProjectSvg;
-
-
-
-
-
-
-
