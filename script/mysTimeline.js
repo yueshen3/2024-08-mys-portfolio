8d4f73d857719eea4520b3f0d3ab3c8de7773306
Yueshen <68021595+yueshen3@users.noreply.github.com>
2024-10-02 23:30:00 -0500

    Update mysTimeline.js

--- script/mysTimeline.js
+++ script/mysTimeline.js
@@ -9,7 +9,7 @@ for (let i = 0; i <= dataEndYear - dataStartYear; i++) {
   recordYear ++;
 }
 
-const yearWidth = 12 * dataScalerUnit;
+const yearLength = 12 * dataScalerUnit;
 let domTime = '';
 arrayYears.forEach((e, i) => {
   domTime += `<p>${e}</p>`;
@@ -21,14 +21,19 @@ let htmlSvgVertical = [];
 for (let i = 0; i <= dataEndYear - dataStartYear; i++) {
   const e = `<line x1="${startPointX}" y1="0" 
   x2="${startPointX}" y2="${window.innerHeight}"/>`;
-  startPointX += yearWidth;
+  startPointX += yearLength;
   htmlSvgVertical[i] = e;
 }
 
 let svgHoriStrokeWidth = 20;
 let svgHeight = document.querySelector('.time-line').offsetHeight;
 const htmlSvgHorizontal = `<line x1="0" y1="${svgHeight - svgHoriStrokeWidth / 2}" 
-x2="${window.innerWidth}" y2="${svgHeight - svgHoriStrokeWidth / 2}" 
+x2="${(dataEndYear - dataStartYear + 1) * yearLength}" 
+y2="${svgHeight - svgHoriStrokeWidth / 2}" 
 style = "stroke-width: ${svgHoriStrokeWidth};"/>`
 document.querySelector('.svg-lines').innerHTML = htmlSvgVertical + htmlSvgHorizontal;
 document.querySelector('.svg-lines').setAttribute("height", `${svgHeight}`)
+
+
+document.body.style.height = 3750 + "px";
+document.body.style.width = (dataEndYear - dataStartYear + 1) * yearLength + "px";
