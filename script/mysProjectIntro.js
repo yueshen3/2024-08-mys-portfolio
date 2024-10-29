b5423096cfdd1a4ff5099d33719dc692bf183d55
yueshen3 <vanklinwoodic@gmail.com>
2024-10-28 23:13:34 -0500

    v_2024-1028_projectIntro

--- script/mysProjectIntro.js
+++ script/mysProjectIntro.js
@@ -7,19 +7,24 @@ console.log(dataInfo);
 
 const dataInfoIndex = dataInfo.findIndex((e) => Number(e.projectID.replaceAll(" ", "")) 
 == Number(projectID.replaceAll(" ", "")));
+const projectLocation = dataInfo[dataInfoIndex]["location"];
+const projectDiscipline = dataInfo[dataInfoIndex]["discipline"];
+const projectGArea = dataInfo[dataInfoIndex]["gross floor area_sqf"];
+const projectProgram = dataInfo[dataInfoIndex]["programs"];
 const projectDescription = dataInfo[dataInfoIndex].discription;
-// const projectDiscipline = dataInfo[dataInfoIndex].discipline;
-const projectArea = dataInfo[dataInfoIndex]["gross floor area_sqf"];
-console.log(projectDescription);
-console.log(projectArea);
+// console.log(projectDescription);
 
 
 let htmlIntroCard = `<img src="image/${projectID.replaceAll(' ', '')}_0000.png">
-<ul>
-<li>Project Code : ${projectID}</li>
-<li>Project Name : ${projectName}</li>
-<li>Project Description : ${projectDescription}</li>
-</ul>
+<table><tbody>
+<tr><th>Code</th><td>${projectID}</td></tr>
+<tr><th>Name</th><td>${projectName}</td></tr>
+<tr><th>Location</th><td>${projectLocation}</td></tr>
+<tr><th>Discipline</th><td>${projectDiscipline}</td></tr>
+<tr><th>GFA (square feet)</th><td>${projectGArea}</td></tr>
+<tr><th>Program</th><td>${projectProgram}</td></tr>
+<tr><th>Description</th><td>${projectDescription}</td></tr>
+</table></tbody>
 `;
 
 document.querySelector('.wrapper-project-card').innerHTML = htmlIntroCard;
