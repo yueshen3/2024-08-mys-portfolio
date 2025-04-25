a6a3e91b272133f9120abd3ac6ffa67d35d1baff
yueshen3 <vanklinwoodic@gmail.com>
2025-04-24 22:50:28 -0500

    v2025-0424_biographyUpdate

--- script/mysProjectsGallery.js
+++ script/mysProjectsGallery.js
@@ -6,8 +6,8 @@ curatedProjectID.forEach((e, i) => {
   htmlProjectCard += `<a href="mys-project-intro.html?projectID=${e}
   &projectName=${curatedProjectName[i]}">
   <img src="image/${e.replaceAll(" ", "")}_0000.png">
-  <h1>${curatedProjectID[i]}</h1>
-  <h1>${curatedProjectName[i]}</h1>
+  <p>${curatedProjectID[i]}</p>
+  <p>${curatedProjectName[i]}</p>
   </a>`;
 })
 document.querySelector(".wrapper-projects").innerHTML = htmlProjectCard;
